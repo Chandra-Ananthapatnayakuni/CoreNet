27a187535485f8fa0bd560ce84ad201e3fa30b2a
BinduShankar <46102128+BinduShankar@users.noreply.github.com>
2018-12-26 10:40:09 +0530

    Add files via upload

---
+++ PCFSalesforceService/Controllers/FPDPrintController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PCFSalesforceService.Model;
+
+namespace PCFSalesforceService.Controllers
+{
+    [Route("api/fpdprints")]
+    [ApiController]
+    public class FPDPrintController : ControllerBase
+    {
+        [HttpPost("")]
+        public HttpResponse Post([FromBody] FPDFields fpdField)
+        {            
+            try
+            {
+                if (fpdField == null)
+                {
+                    return NotFound();
+                }
+                return Ok();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        [HttpGet("")]
+        public string Get()
+        {
+            return "Success";
+        }
+    }
+}
---
+++ PCFSalesforceService/Model/FPDFields.cs
@@ -0,0 +1,37 @@
+﻿namespace PCFSalesforceService.Model
+{
+    public class FPDFields
+    {
+        public string record_id { get; set; }
+        public char iscontactcentreuser { get; set; }
+        public string policy_number { get; set; }
+        public char transfer { get; set; }
+        public string transfer_type { get; set; }
+        public char plan_details { get; set; }
+        public string prod_nme { get; set; }
+        public int case_id { get; set; }
+        public char charges { get; set; }
+        public char current_value { get; set; }
+        public char schedule_of_contributions { get; set; }
+        public char tax_breakdown { get; set; }
+        public int no_of_illustrations { get; set; }
+        public string additional_letter_text { get; set; }
+        public char dd_instrustions { get; set; }
+        public char fund_switch_form { get; set; }
+        public string customer_type { get; set; }
+        public string company_name { get; set; }
+        public string contact_name { get; set; }
+        public string address_line1 { get; set; }
+        public string address_line2 { get; set; }
+        public string address_city { get; set; }
+        public string address_county { get; set; }
+        public string postcode { get; set; }
+        public string reference { get; set; }
+        public char servicing_agent_copy { get; set; }
+        public char print_only { get; set; }
+        public char archive_ind { get; set; }
+        public string request_status { get; set; }
+        public char request_to_print { get; set; }
+        public char request_submitted { get; set; }
+    }
+}
